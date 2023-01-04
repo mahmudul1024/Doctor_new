abb8d98ddeeacd0298a7a9258ca320e872d2db67
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2023-01-04 13:35:10 +0100

    usenavigation added on payment

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -4,15 +4,21 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 import "./Payment.css";
 import CheckoutForm from "./CheckoutForm";
+import { useNavigation } from "react-day-picker";
 const stripePromise = loadStripe(
   "pk_test_51MMJHSFwyc4dxaOOKlOlx0tLSyXDZpvPjJWvxmTtU189VIogBoveT2TfPcJSQB6AJk6Mqj4rUEmqLtLUtTxMEgFe00A4gjmL8c"
 );
 console.log(stripePromise);
 const Payment = () => {
+  const navigation = useNavigation();
   const booking = useLoaderData();
   const { treatment, appointmentDate, slot, price } = booking;
 
   console.log("payment data", price);
+
+  if (navigation.state === "loading") {
+    return <progress className="progress w-56"></progress>;
+  }
   return (
     <div>
       <h3 className=" text-3xl py-3"> Payment for {treatment} </h3>
